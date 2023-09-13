c628517aa0e4848010b44b24aa3ec7bea2d6b1d1
korulla <korulla@users.noreply.github.com>
2023-09-13 18:39:29 +0530

    completed

--- functions/Javascript/homeNav.js
+++ functions/Javascript/homeNav.js
@@ -2,5 +2,5 @@ const button = document.getElementById('homeButton');
 
 button.addEventListener('click', function() {
     // Redirect to the target HTML page in another folder
-    window.location.href = '../../index.html';
+    window.location.href = '../../../index.html';
 });
